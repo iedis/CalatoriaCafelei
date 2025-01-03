ad5acd990a28d2c28994f6eb28b68014108721f7
Edis Perchiata <edisperchiata@yahoo.com>
2025-01-03 21:15:20 +0200

    add getters and setters

--- coffeehouse.hpp
+++ coffeehouse.hpp
@@ -17,7 +17,86 @@
             std::vector<Order> myOrders;
         public:
 //getters and setters
-
+            void setManager(Manager manager)
+            {
+                this->myManager = manager;
+            }
+            void setOneBarista(Barista barista)
+            {
+                this->myBaristas.push_back(barista);
+            }
+            void setBaristas(std::vector<Barista> baristas)
+            {
+                this->myBaristas = baristas;
+            }
+            void setOneWaiter(Waiter waiter)
+            {
+                this->myWaiters.push_back(waiter);
+            }
+            void setWaiters(std::vector<Waiter> waiters)
+            {
+                this->myWaiters = waiters;
+            }
+            void setOneWage(int wage)
+            {
+                this->myWages.push_back(wage);
+            }
+            void setWages(std::vector<int> wages)
+            {
+                this->myWages = wages;
+            }
+            void setStock(Stock myStock)
+            {
+                this->myStock = myStock;
+            }
+            void setOneOrder(Order order)
+            {
+                this->myOrders.push_back(order);
+            }
+            void setOrders(std::vector<Order> orders)
+            {
+                this->myOrders = orders;
+            }
+            Manager getManager()
+            {
+                return this->myManager;
+            }
+            Barista getOneBarista(int index)
+            {
+                return this->myBaristas[index];
+            }
+            std::vector<Barista> getBaristas()
+            {
+                return this->myBaristas;
+            }
+            Waiter getOneWaiter(int index)
+            {
+                return this->myWaiters[index];
+            }
+            std::vector<Waiter> getWaiters()
+            {
+                return this->myWaiters;
+            }
+            int getOneWage(int index)
+            {
+                return this->myWages[index];
+            }
+            std::vector<int> getWages()
+            {
+                return this->myWages;
+            }
+            Stock getStock()
+            {
+                return this->myStock;
+            }
+            Order getOneOrder(int index)
+            {
+                return this->myOrders[index];
+            }
+            std::vector<Order> getOrders()
+            {
+                return this->myOrders;
+            }
 //hire new empolyee
             template <typename T>
             void hireEmployee(T newEmployee)
